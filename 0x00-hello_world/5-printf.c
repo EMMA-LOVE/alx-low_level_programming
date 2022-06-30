ae9b939470dba04615b7e6a0fc4ec6377790eb19
EMMA-LOVE <oyeniyi978@gmail.com>
2022-06-30 14:43:16 -0700

    Ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
+/**
+  * main -entry point
+  * Return: Always 0 (sucess)
+  */
+
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
