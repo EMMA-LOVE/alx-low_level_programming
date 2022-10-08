0c37d50349888408bd676d452e68cd3700b8f1ae
EMMA-LOVE <oyeniyi978@gmail.com>
2022-10-08 05:08:40 -0700

    t

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
+
+}
